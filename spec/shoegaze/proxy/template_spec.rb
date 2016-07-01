41a523f0be6d3c226d228d3e879e002596072fb0
Dan Connor <danconn@danconnor.com>
2016-07-01 15:54:55 -0400

    proxy template spec

---
+++ spec/shoegaze/proxy/template_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Shoegaze::Proxy::Template do
+  let!(:class_double){ double }
+  let!(:instance_double){ double }
+
+  before :each do
+    Shoegaze::Proxy::Template.class_double = class_double
+    Shoegaze::Proxy::Template.instance_double = instance_double
+  end
+
+  describe "instance method calls" do
+    it "get delegated to the instance double" do
+      expect(instance_double).to receive(:rights).and_return(:liberties)
+      expect(Shoegaze::Proxy::Template.new.rights).to eq(:liberties)
+    end
+  end
+
+  describe "class method calls" do
+    it "get delegated to the class double" do
+      expect(class_double).to receive(:government).and_return(:nothing_of_value)
+      expect(Shoegaze::Proxy::Template.government).to eq(:nothing_of_value)
+    end
+  end
+end
